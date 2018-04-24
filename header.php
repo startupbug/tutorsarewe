0fbed0478238543a0a28a0914c39da4189edf930
farehaiqbal <fariha.aimviz@gmail.com>
2018-04-24 15:01:00 +0500

    first Commit

---
+++ header.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+      <title>TutorAreUs</title>
+      <!-- Bootstrap -->
+      <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+      <!-- FontAwesome -->
+      <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+      <!-- Animate -->
+      <link href="assets/css/animate.css" rel="stylesheet">
+      <!-- Owl Slider -->
+      <link href="assets/css/owl.carousel.css" rel="stylesheet">
+      <!-- Owl Slider Theme -->
+      <link href="assets/css/owl.theme.css" rel="stylesheet">
+      <!--Jquery Validation -->
+      <link href="assets/css/validationEngine.jquery.css" rel="stylesheet">
+      <!--Jquery custom Validation -->
+      <link href="assets/css/custom_validatiion.css" rel="stylesheet">
+      <!--Mobile menu -->
+      <!--  <link href="assets/plugins/menu/css/hamburgers.css" rel="stylesheet">
+         <link href="assets/plugins/menu/css/jquery.mmenu.all.css" rel="stylesheet">
+         <link href="assets/plugins/menu/css/jquery.mhead.css" rel="stylesheet"> -->
+      <!-- Animation CSS -->
+      <!-- <link rel="stylesheet" type="text/css" href="assets/css/default.css" />
+         <link rel="stylesheet" type="text/css" href="assets/css/component.css" /> -->
+      <!-- AOS Animation -->
+      <link href="assets/css/aos.css" rel="stylesheet">
+      <!-- style.css -->
+      <link href="assets/css/style.css" rel="stylesheet">
+      <!-- custom Css Lins -->
+      <link href="assets/css/custom.css" rel="stylesheet">
+      <!-- Responsive -->
+      <link href="assets/css/responsive.css" rel="stylesheet">
+   </head>
+   <body>
+      <div id="wrapper">
+      <header>
+         <div class="container">
+            <div class="row">
+               <div class="top-header">
+                  <ul class="top-header-nav">
+                     <li>:  1-877-3- TUTORS 1877-388-8677</li>
+                     <li class="f_right">: Search For Tutors</li>
+                     <li class="f_right">: Login/Register</li>
+                  </ul>
+               </div>
+               <div class="col-md-12 col-sm-12 col-xs-12">
+                  <nav class="navbar navbar-inverse">
+                     <div class="container-fluid">
+                        <div class="navbar-header">
+                           <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                           <span class="icon-bar"></span>
+                           <span class="icon-bar"></span>
+                           <span class="icon-bar"></span>                        
+                           </button>
+                           <a href="index.php"><img src="assets/images/logo.png" alt="tutorareus Logo" class="img-responsive"></a>
+                        </div>
+                        <div class="collapse navbar-collapse" id="myNavbar">
+                           <ul class="nav navbar-nav header-nav">
+                              <li class="dropdown">
+                                 <a class="dropdown-toggle f_dropdown" data-toggle="dropdown" href="#">FIND A TUTOR  <span class="glyphicon glyphicon-chevron-down text-muted f_icon"></span></a>
+                                 <ul class="dropdown-menu">
+                                    <li><a href="#">Page 1-1</a></li>
+                                    <li><a href="#">Page 1-2</a></li>
+                                    <li><a href="#">Page 1-3</a></li>
+                                 </ul>
+                              </li>
+                              <li class="dropdown">
+                                 <a class="dropdown-toggle f_dropdown" data-toggle="dropdown" href="#">HOW IT WORKS  <span class="glyphicon glyphicon-chevron-down text-muted f_icon"></span></a>
+                                 <ul class="dropdown-menu">
+                                    <li><a href="#">Page 1-1</a></li>
+                                    <li><a href="#">Page 1-2</a></li>
+                                    <li><a href="#">Page 1-3</a></li>
+                                 </ul>
+                              </li>
+                              <li class="dropdown">
+                                 <a class="dropdown-toggle f_dropdown" data-toggle="dropdown" href="#">START TUTORING  <span class="glyphicon glyphicon-chevron-down text-muted f_icon"></span></a>
+                                 <ul class="dropdown-menu">
+                                    <li><a href="#">Page 1-1</a></li>
+                                    <li><a href="#">Page 1-2</a></li>
+                                    <li><a href="#">Page 1-3</a></li>
+                                 </ul>
+                              </li>
+                              <li class="dropdown">
+                                 <a class="dropdown-toggle f_dropdown" data-toggle="dropdown" href="#">BECOME A TUTOR  <span class="glyphicon glyphicon-chevron-down text-muted f_icon"></span></a>
+                                 <ul class="dropdown-menu">
+                                    <li><a href="#">Page 1-1</a></li>
+                                    <li><a href="#">Page 1-2</a></li>
+                                    <li><a href="#">Page 1-3</a></li>
+                                 </ul>
+                              </li>
+                              <li class="dropdown">
+                                 <a class="dropdown-toggle f_dropdown" data-toggle="dropdown" href="#">PUBLICATIONS  <span class="glyphicon glyphicon-chevron-down text-muted f_icon"></span></a>
+                                 <ul class="dropdown-menu">
+                                    <li><a href="#">Page 1-1</a></li>
+                                    <li><a href="#">Page 1-2</a></li>
+                                    <li><a href="#">Page 1-3</a></li>
+                                 </ul>
+                              </li>
+                              <li class="dropdown">
+                                 <a class="dropdown-toggle f_dropdown" data-toggle="dropdown" href="#">ABOUT US  <span class="glyphicon glyphicon-chevron-down text-muted f_icon"></span></a>
+                                 <ul class="dropdown-menu">
+                                    <li><a href="#">Page 1-1</a></li>
+                                    <li><a href="#">Page 1-2</a></li>
+                                    <li><a href="#">Page 1-3</a></li>
+                                 </ul>
+                              </li>
+                           </ul>
+                        </div>
+                     </div>
+                  </nav>
+               </div>
+            </div>
+         </div>
+      </header>
---
+++ index.php
@@ -0,0 +1,278 @@
+<?php include 'header.php'; ?>
+<section class="tutor">
+   <div class="container-fluid">
+      <div class="row">
+         <div class="col-md-12 f_padding">
+            <div class="img_banner">
+               <img src="assets/images/banner2.png" class="img-responsive">
+            </div>
+            <div class="header-text">
+               <div class="col-md-12 text-center">
+                  <h2>
+                     Online Learning Anytime, Anywhere!
+                  </h2>
+                  <br>
+                  <h3>
+                     Search - Register - Excel
+                  </h3>
+                  <p>Tutors Are US is a subsidiary of Available Group LLC. We are here to meet your needs. Our teaching philosophy is <br>teaching with fun and to create “A” student. Lets turn you into an A student in 3 easy steps</p>
+                  <br>
+                  <div class="">
+                     <a class="btn btn-theme btn-sm btn-min-block f_about" href="#">ABOUT US</a><a class="btn btn-theme btn-sm btn-min-block f_about" href="#">CONTACT US</a>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+   <div class="container">
+      <div class="row">
+         <div class="col-md-12 f_main">
+            <h3 class="f_content">What are you looking for?</h3>
+            <hr>
+            <section id="tabs">
+               <nav>
+                  <div class="nav nav-tabs nav-fill f_nav" id="nav-tab" role="tablist">
+                     <div class="col-md-3 col-md-offset-1">
+                        <a class="nav-item nav-link f_tab" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true"><i class="fa fa-home f_home" aria-hidden="true"></i>Home Tutoring</a>
+                     </div>
+                     <div class="col-md-3 col-md-offset-1">
+                        <a class="nav-item nav-link f_tab" id="nav-contact-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false"><i class="fa fa-desktop f_home"></i>Online Tutoring</a>
+                     </div>
+                     <div class="col-md-3 col-md-offset-1">
+                        <a class="nav-item nav-link f_tab" id="nav-about-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false"><i class="fa fa-hourglass-half f_home"></i>Full Time</a>
+                     </div>
+                  </div>
+               </nav>
+               <div class="tab-content py-3 px-3 px-sm-0" id="nav-tabContent">
+                  <div class="tab-pane fade active in" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+                     <form>
+                        <div class="form-group col-md-6 f_bottom">
+                           <select class="form-control f_color" id="emailFrom" name="emailFrom">
+                              <option>Type of Course</option>
+                              <option value="Sergio Rodriguez|sergio.rodriguez@tix.com">Sergio Rodriguez (sergio.rodriguez@tix.com)</option>
+                              <option value="Silvia Mahoney|silvia.mahoney@tix.com">Silvia Mahoney (sergio.rodriguez@tix.com)</option>
+                              <option value="Steve Moore|steve.moore@tix.com">Steve Moore (sergio.rodriguez@tix.com)</option>
+                              <option value="Luke Perria|luke.perria@tix.com">Adam Hettinger (sergio.rodriguez@tix.com)</option>
+                              <option value="Luke Perria|luke.perria@tix.com">Luke Perea (sergio.rodriguez@tix.com)</option>
+                           </select>
+                        </div>
+                        <div class="form-group col-md-3 f_bottom">
+                           <select class="form-control f_color" id="emailFrom" name="emailFrom">
+                              <option>Select Location</option>
+                              <option value="Sergio Rodriguez|sergio.rodriguez@tix.com">Sergio Rodriguez (sergio.rodriguez@tix.com)</option>
+                              <option value="Silvia Mahoney|silvia.mahoney@tix.com">Silvia Mahoney (sergio.rodriguez@tix.com)</option>
+                              <option value="Steve Moore|steve.moore@tix.com">Steve Moore (sergio.rodriguez@tix.com)</option>
+                              <option value="Luke Perria|luke.perria@tix.com">Adam Hettinger (sergio.rodriguez@tix.com)</option>
+                              <option value="Luke Perria|luke.perria@tix.com">Luke Perea (sergio.rodriguez@tix.com)</option>
+                           </select>
+                        </div>
+                        <div class="col-md-3 f_bottom"><button type="submit" class="btn btn-default f_color button_tour">SEARCH TOUR</button></div>
+                     </form>
+                  </div>
+                  <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-contact-tab">
+                     <form>
+                        <div class="form-group col-md-6 f_bottom">
+                           <select class="form-control f_color" id="emailFrom" name="emailFrom">
+                              <option>Type of Course</option>
+                              <option value="Sergio Rodriguez|sergio.rodriguez@tix.com">Sergio Rodriguez (sergio.rodriguez@tix.com)</option>
+                              <option value="Silvia Mahoney|silvia.mahoney@tix.com">Silvia Mahoney (sergio.rodriguez@tix.com)</option>
+                              <option value="Steve Moore|steve.moore@tix.com">Steve Moore (sergio.rodriguez@tix.com)</option>
+                              <option value="Luke Perria|luke.perria@tix.com">Adam Hettinger (sergio.rodriguez@tix.com)</option>
+                              <option value="Luke Perria|luke.perria@tix.com">Luke Perea (sergio.rodriguez@tix.com)</option>
+                           </select>
+                        </div>
+                        <div class="form-group col-md-3 f_bottom">
+                           <select class="form-control f_color" id="emailFrom" name="emailFrom">
+                              <option>Select Location</option>
+                              <option value="Sergio Rodriguez|sergio.rodriguez@tix.com">Sergio Rodriguez (sergio.rodriguez@tix.com)</option>
+                              <option value="Silvia Mahoney|silvia.mahoney@tix.com">Silvia Mahoney (sergio.rodriguez@tix.com)</option>
+                              <option value="Steve Moore|steve.moore@tix.com">Steve Moore (sergio.rodriguez@tix.com)</option>
+                              <option value="Luke Perria|luke.perria@tix.com">Adam Hettinger (sergio.rodriguez@tix.com)</option>
+                              <option value="Luke Perria|luke.perria@tix.com">Luke Perea (sergio.rodriguez@tix.com)</option>
+                           </select>
+                        </div>
+                        <div class="col-md-3 f_bottom"><button type="submit" class="btn btn-default f_color button_tour">SEARCH TOUR</button></div>
+                     </form>
+                  </div>
+                  <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-about-tab">
+                     <form>
+                        <div class="form-group col-md-6 f_bottom">
+                           <select class="form-control f_color" id="emailFrom" name="emailFrom">
+                              <option>Type of Course</option>
+                              <option value="Sergio Rodriguez|sergio.rodriguez@tix.com">Sergio Rodriguez (sergio.rodriguez@tix.com)</option>
+                              <option value="Silvia Mahoney|silvia.mahoney@tix.com">Silvia Mahoney (sergio.rodriguez@tix.com)</option>
+                              <option value="Steve Moore|steve.moore@tix.com">Steve Moore (sergio.rodriguez@tix.com)</option>
+                              <option value="Luke Perria|luke.perria@tix.com">Adam Hettinger (sergio.rodriguez@tix.com)</option>
+                              <option value="Luke Perria|luke.perria@tix.com">Luke Perea (sergio.rodriguez@tix.com)</option>
+                           </select>
+                        </div>
+                        <div class="form-group col-md-3 f_bottom">
+                           <select class="form-control f_color" id="emailFrom" name="emailFrom">
+                              <option>Select Location</option>
+                              <option value="Sergio Rodriguez|sergio.rodriguez@tix.com">Sergio Rodriguez (sergio.rodriguez@tix.com)</option>
+                              <option value="Silvia Mahoney|silvia.mahoney@tix.com">Silvia Mahoney (sergio.rodriguez@tix.com)</option>
+                              <option value="Steve Moore|steve.moore@tix.com">Steve Moore (sergio.rodriguez@tix.com)</option>
+                              <option value="Luke Perria|luke.perria@tix.com">Adam Hettinger (sergio.rodriguez@tix.com)</option>
+                              <option value="Luke Perria|luke.perria@tix.com">Luke Perea (sergio.rodriguez@tix.com)</option>
+                           </select>
+                        </div>
+                        <div class="col-md-3 f_bottom"><button type="submit" class="btn btn-default f_color button_tour">SEARCH TOUR</button></div>
+                     </form>
+                  </div>
+               </div>
+            </section>
+         </div>
+      </div>
+   </div>
+   <section class="info">
+      <div class="container">
+         <div class="row">
+            <div class="col-md-4 col-md-offset-0 col-sm-6 col-xs-12 text-center">
+               <img src="assets/images/image1.png" class="img-responsive info_img">
+               <h3 class="f_online">Online and Offline</h3>
+               <p class="f_meet">We can meet the student online or we can come <br>to your home or designated location. You don’t <br>have to worry. We have tutors ASAP (AS SOON <br>AS POSSIBLE) and PRN (AS needed). We can <br>help you or your child succeed.</p>
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-12 text-center">
+               <img src="assets/images/image1.png" class="img-responsive info_img">
+               <h3 class="f_online">Online and Offline</h3>
+               <p class="f_meet">We can meet the student online or we can come <br>to your home or designated location. You don’t <br>have to worry. We have tutors ASAP (AS SOON <br>AS POSSIBLE) and PRN (AS needed). We can <br>help you or your child succeed.</p>
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-12 text-center col-md-offset-0 col-sm-offset-3 col-xs-offset-0">
+               <img src="assets/images/image1.png" class="img-responsive info_img">
+               <h3 class="f_online">Online and Offline</h3>
+               <p class="f_meet">We can meet the student online or we can come <br>to your home or designated location. You don’t <br>have to worry. We have tutors ASAP (AS SOON <br>AS POSSIBLE) and PRN (AS needed). We can <br>help you or your child succeed.</p>
+            </div>
+         </div>
+      </div>
+   </section>
+</section>
+<section class="check">
+   <div class="container-fluid">
+      <div class="row">
+         <div class="col-md-4 col-md-offset-1">
+            <h3 class="f_check">We got you,<br> we can help</h3>
+            <p class="f_per">Sed ut perspiciatis unde omnis iste natus error sit voluptatem <br> accusantium doloremque laudantium, totam rem aperiam, eaque ipsa <br>quae ab illo inventore veritatis et quasi architecto beatae vitae dicta <br>sunt explicabo. Nemo enim ipsam volups protatem quia voluptas sit <br>aspernatur aut odit aut fugit.</p>
+            <div class="btn_check"><a href="">CHECK ALL COURSES</a></div>
+         </div>
+         <div class="col-md-7">
+            <div class="row">
+               <div class="col-md-4" style="padding: 0px;">
+                  <div class="bg_green">
+                     <div class="f_math">
+                        <img src="assets/images/maths.png" class="img-responsive img_math">
+                        <p class="math">Mathematics</p>
+                     </div>
+                  </div>
+                  <div class="bg_light_blue">
+                     <div class="f_math">
+                        <img src="assets/images/language.png" class="img-responsive img_math">
+                        <p class="math">Languages</p>
+                     </div>
+                  </div>
+                  <div class="bg_organ">
+                     <div class="f_math">
+                        <img src="assets/images/atom.png" class="img-responsive img_math">
+                        <p class="math">Science</p>
+                     </div>
+                  </div>
+                  <div class="bg_sky_blue">
+                     <div class="f_math">
+                        <img src="assets/images/bookmark.png" class="img-responsive img_math">
+                        <p class="math">Other Subjects</p>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-md-8 f_padding1">
+                  <img src="assets/images/info_img.png" class="img-responsive f_img1">
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<section class="images_courses">
+   <div class="container">
+      <div class="row">
+         <h3 class="added_course">Recent Added Courses</h3>
+         <div class="col-md-4 col-sm-6 col-xs-12 text-center">
+            <img src="assets/images/images1.png" class="img-responsive">
+            <h3 class="full_time">Full Time Tutoring</h3>
+            <p class="f_vul">Voluptatem accusantium doloremque</p>
+         </div>
+         <div class="col-md-4 col-sm-6 col-xs-12 text-center">
+            <img src="assets/images/images2.png" class="img-responsive">
+            <h3 class="full_time">Science Fair Help</h3>
+            <p class="f_vul">Voluptatem accusantium doloremque</p>
+         </div>
+         <div class="col-md-4 col-sm-6 col-xs-12 text-center col-md-offset-0 col-sm-offset-3 col-xs-offset-0">
+            <img src="assets/images/images3.png" class="img-responsive">
+            <h3 class="full_time">Social Studies</h3>
+            <p class="f_vul">Voluptatem accusantium doloremque</p>
+         </div>
+      </div>
+      <div class="row">
+         <div class="col-md-4 col-sm-6 col-xs-12 text-center">
+            <img src="assets/images/images1.png" class="img-responsive">
+            <h3 class="full_time">Full Time Tutoring</h3>
+            <p class="f_vul">Voluptatem accusantium doloremque</p>
+         </div>
+         <div class="col-md-4 col-sm-6 col-xs-12 text-center">
+            <img src="assets/images/images2.png" class="img-responsive">
+            <h3 class="full_time">Science Fair Help</h3>
+            <p class="f_vul">Voluptatem accusantium doloremque</p>
+         </div>
+         <div class="col-md-4 col-sm-6 col-xs-12 text-center col-md-offset-0 col-sm-offset-3 col-xs-offset-0">
+            <img src="assets/images/images3.png" class="img-responsive">
+            <h3 class="full_time">Social Studies</h3>
+            <p class="f_vul">Voluptatem accusantium doloremque</p>
+         </div>
+      </div>
+   </div>
+</section>
+<section class="f_choose">
+   <div class="container-fluid">
+      <div class="row">
+         <div class="col-md-6 f_side_padding">
+            <img src="assets/images/computer1.png" class="img-responsive">
+         </div>
+         <div class="col-md-6">
+            <h3 class="f_text">Why choose us?</h3>
+            <p class="f_tutor"><i class="fa fa-check"></i>We conduct a pretest to know where the student stands</p>
+            <br>
+            <p class="f_tutor"><i class="fa fa-check"></i>We tutor based on the pre test and specific child needs</p>
+            <br>
+            <p class="f_tutor"><i class="fa fa-check"></i>We have qualified tutors to meet your needs</p>
+            <br>
+            <p class="f_tutor"><i class="fa fa-check"></i> 24/access - Find a tutor anytime, anywhere, using our online tutorial</p>
+            <div class="btn_check"><a href="#">HOW DOES THIS WORK</a></div>
+         </div>
+      </div>
+   </div>
+</section>
+<section class="logo_f">
+   <div class="container">
+      <h3 class="added_course">Featured on</h3>
+      <div class="row">
+         <div class="col-md-2 col-sm-6 col-xs-6">
+            <img src="assets/images/logo1.png" class="img-responsive">
+         </div>
+         <div class="col-md-2 col-sm-6 col-xs-6">
+            <img src="assets/images/logo2.png" class="img-responsive">
+         </div>
+         <div class="col-md-2 col-sm-6 col-xs-6">
+            <img src="assets/images/logo3.png" class="img-responsive">
+         </div>
+         <div class="col-md-2 col-sm-6 col-xs-6">
+            <img src="assets/images/logo4.png" class="img-responsive">
+         </div>
+         <div class="col-md-2 col-sm-6 col-xs-6">
+            <img src="assets/images/logo5.png" class="img-responsive">
+         </div>
+         <div class="col-md-2 col-sm-6 col-xs-6">
+            <img src="assets/images/logo6.png" class="img-responsive">
+         </div>
+      </div>
+   </div>
+</section>
+<?php include 'footer.php'; ?>
