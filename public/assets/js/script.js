b7115e112ae5f634c4e4fdd8ca617ea1f541cedb
unknown <Sheheryar.n@Soft-20-PC.aimviz.com>
2018-05-30 17:30:42 +0500

    Push

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -3,48 +3,48 @@ $(document).ready(function () {
 
 
   // call validation on form
-  $("#Sign_up_form").validate();
-
-  //contact form validation
-  $("#contact_form").validate();
-   //signin form validation
-
-  $("#signin_form").validate();
-    
-     //signin form validation
-
-  $("#registration_form").validate();
-
-    //monthly cost
-
-
-  $("#monthlycost_form").validate();
+  // $("#Sign_up_form").validate();
+  //
+  // //contact form validation
+  // $("#contact_form").validate();
+  //  //signin form validation
+  //
+  // $("#signin_form").validate();
+  //
+  //    //signin form validation
+  //
+  // $("#registration_form").validate();
+  //
+  //   //monthly cost
+  //
+  //
+  // $("#monthlycost_form").validate();
 });
 
 
 $(function() {
-        
-  $("#my-menu").mmenu({
-    extensions  : [ "shadow-panels", "fx-panels-slide-100", "border-none", "theme-black", "fullscreen" ],
-    navbars   : {
-      content : [ "prev", "<img src='assets/images/logo_actor.png' class='mobile-logo' />", "close" ],
-      height  : 2
-    },
-    setSelected: true,
-    searchfield: {
-      resultsPanel: true
-    }}, { 
-  });
 
-  $(".mh-head.mm-sticky").mhead({
-    scroll: {
-      hide: 200
-    }
-  });
-
-  $(".mh-head:not(.mm-sticky)").mhead({
-    scroll: false
-  });
+  // $("#my-menu").mmenu({
+  //   extensions  : [ "shadow-panels", "fx-panels-slide-100", "border-none", "theme-black", "fullscreen" ],
+  //   navbars   : {
+  //     content : [ "prev", "<img src='assets/images/logo_actor.png' class='mobile-logo' />", "close" ],
+  //     height  : 2
+  //   },
+  //   setSelected: true,
+  //   searchfield: {
+  //     resultsPanel: true
+  //   }}, {
+  // });
+  //
+  // $(".mh-head.mm-sticky").mhead({
+  //   scroll: {
+  //     hide: 200
+  //   }
+  // });
+  //
+  // $(".mh-head:not(.mm-sticky)").mhead({
+  //   scroll: false
+  // });
 
   $('body').on( 'click',
     'a[href^="#/"]',
@@ -56,5 +56,3 @@ $(function() {
 });
 
 //for custom javascript functions function
-
-
