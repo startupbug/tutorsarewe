9d7d7ca2ec086e49618c4dd5eb4183fb5c619a11
Irfan Mumtaz <Irfan.m@aimviz.com>
2018-07-31 11:31:18 +0500

    invite friends

--- resources/views/dashboard/invite_friend.blade.php
+++ resources/views/dashboard/invite_friend.blade.php
@@ -1,9 +1,21 @@
 @extends('dashboard.dashboard-app')
 @section('content')
-	<form action="{{route('invite_request')}}" method="post">
-		{{csrf_field()}}
-		<label>Email</label>
-		<input type="email" name="email">
-		<input type="submit" name="submit">
-	</form>
+<section class="profile">
+   <div class="container-fluid remove_padding bg_color_gray">
+  		@include('dashboard.partials.dashboard-sidebar')
+   		<div class="col-md-9 bg_color">
+			<form action="{{ route('invite_request') }}" method="post" class="form-groups">
+
+				{{ csrf_field() }}
+				
+				<label>Email</label>
+
+				<input type="email" class="form-control" name="email" placeholder="Enter Email Address">
+
+				<input type="submit" name="submit">
+
+			</form>
+   		</div>
+	</div>
+</section>
 @endsection()
